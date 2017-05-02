bd2a561b63ebb6e1376a10cc5b42bdb6507be669
Jure Cuhalev <jure@cuhalev.com>
2017-05-02 16:49:47 +0200

    Hack in support for 'data-v-offset', 'data-h-offset'

--- addon/components/zf-dropdown.js
+++ addon/components/zf-dropdown.js
@@ -11,7 +11,8 @@ export default Ember.Component.extend(zfWidget, {
   classNameBindings: ['_position'],
 
   /** @member Attribute bindings */
-  attributeBindings: ['data-dropdown', 'data-auto-focus', 'data-hover', 'data-hover-pane'],
+  attributeBindings: ['data-dropdown', 'data-auto-focus', 'data-hover', 'data-hover-pane',
+                      'data-v-offset', 'data-h-offset'],
 
   /** @member Makes the data attribute binding appear */
   'data-dropdown': ' ',
